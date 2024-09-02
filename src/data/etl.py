5270cda9ca80200789e5a0381910296881c2d6f0
Breno Ferrari <brenoferrari.0208@gmail.com>
2024-09-01 21:36:07 -0300

    finalização módulo de ajuste de tipos dos dados INMET

--- src/data/etl.py
+++ src/data/etl.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 import pandas as pd
+import numpy as np
 
 def descompacta_zip(dir_zip, dir_ext):
     with zipfile.ZipFile(dir_zip, 'r') as zip_ref:
@@ -24,13 +25,81 @@ def processa_csv_inmet(df_info, df):
 
 def transformacao_inmet(df):
     # Colunas
+    dict_ajt_var = {'data (yyyy-mm-dd)':'data', 
+        'hora (utc)':'hora', 
+        'região':'regiao', 
+        'uf':'uf', 
+        'estação':'estacao',
+        'codigo (wmo)':'codigo_wmo', 
+        'latitude':'latitude', 
+        'longitude':'longitude', 
+        'altitude':'altitude',
+        'precipitação total, horário (mm)':'precipitacao_total',
+        'pressao atmosferica ao nivel da estacao, horaria (mb)':'pressao_atm_nvl_estac',
+        'pressão atmosferica max.na hora ant. (aut) (mb)':'pressao_atm_max_hora_ant',
+        'pressão atmosferica min. na hora ant. (aut) (mb)':'pressao_atm_min_hora_ant',
+        'radiacao global (kj/m²)':'radiacao_global',
+        'temperatura do ar - bulbo seco, horaria (°c)':'temp_ar_bulbo_seco',
+        'temperatura do ponto de orvalho (°c)':'temp_ponto_orvalho',
+        'temperatura máxima na hora ant. (aut) (°c)':'temp_max_hora_ant',
+        'temperatura mínima na hora ant. (aut) (°c)':'temp_min_hora_ant',
+        'temperatura orvalho max. na hora ant. (aut) (°c)':'temp_orvalho_max_hora_ant',
+        'temperatura orvalho min. na hora ant. (aut) (°c)':'temp_orvalho_min_hora_ant',
+        'umidade rel. max. na hora ant. (aut) (%)':'umidade_rel_max_hora_ant',
+        'umidade rel. min. na hora ant. (aut) (%)':'umidade_rel_min_hora_ant',
+        'umidade relativa do ar, horaria (%)':'umid_rel_ar',
+        'vento, direção horaria (gr) (° (gr))':'vento_dir_hora', 
+        'vento, rajada maxima (m/s)':'vento_raj_max',
+        'vento, velocidade horaria (m/s)':'vento_vel_hora'
+    }
+
     cols = [c for c in df if "Unnamed" not in c]
     df = df[cols]
     df.columns = [c.lower() for c in df]
+    df.columns = df.columns.map(dict_ajt_var)
 
+    # Limpeza
+    lat_long = ['latitude', 'longitude']
 
+    for l in lat_long:
+        df[l] = (df[l]
+                    .astype(str)
+                    .str.replace(',', '.'))
+        
     # Níveis
 
     # Tipo
+    var_float = [
+        'latitude', 
+        'longitude',
+        'pressao_atm_nvl_estac',
+        'pressao_atm_max_hora_ant',
+        'pressao_atm_min_hora_ant',
+        'radiacao_global',
+        'temp_ar_bulbo_seco',
+        'temp_ponto_orvalho',
+        'temp_max_hora_ant',
+        'temp_min_hora_ant',
+        'temp_orvalho_max_hora_ant',
+        'temp_orvalho_min_hora_ant',
+        'vento_raj_max',
+        'vento_vel_hora'
+    ]
+    for f in var_float:
+        df[f] = df[f].astype(np.float64)
 
+    var_int = [
+        'altitude',
+        'umidade_rel_min_hora_ant',
+        'umid_rel_ar',
+        'vento_dir_hora'
+    ]
+    for i in var_int:
+        df[i] = df[i].astype(np.int64)
+    
+    df['data'] = pd.to_datetime(df['data'], format='%Y-%m-%d')
+
+    return df 
+
+def carregamento_inmet(df):
     return df
